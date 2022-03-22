30f675c3701768008e7bbd20c16caf5be1e760d3
Swaakk <swathykrishna.thayyil@gmail.com>
2022-03-22 23:16:16 +0530

    finished aside component

--- src/Components/Aside.js
+++ src/Components/Aside.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import randomColor from "randomcolor";
+
+
 function Aside() {
+  const[color,setColor]=useState("red")
+  const newColor = randomColor;
+  const changeColor=()=>{
+    setColor(newColor)
+  }
   return (
     <div className="aside">
-      <h2> Aside</h2>
-      <button>Change Color</button>
+      <h2 style={{color}}> Aside</h2>
+      <button onClick={changeColor}>Change Color</button>
     </div>
   );
 }
