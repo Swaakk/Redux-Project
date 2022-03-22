7da58f0fda50bc950faca5a1eed6ab1ecef61c1a
Swaakk <swathykrishna.thayyil@gmail.com>
2022-03-22 22:30:06 +0530

    added scss

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -2,13 +2,28 @@ import react,{useState} from "react";
 
 
 function Counter() {
-  const[count,setCount]=useState(3)
+  const[count,setCount]=useState(3);
+  const increment=()=>{
+    setCount(count=>{
+      return(
+        count +1
+      )
+    })
+  }
+
+  const decrement=()=>{
+    setCount(count=>{
+      return(
+        count - 1
+      )
+    })
+  }
   return (
     <div className="counter">
       <h2>Counter</h2>
-      <h3>The count is - 0</h3>
-      <button>Increase</button>
-      <button>Decrease</button>
+      <h3>The count is - <span>{count}</span></h3>
+      <button onClick={increment}>Increase</button>
+      <button onClick={decrement}>Decrease</button>
     </div>
   ); 
 }
